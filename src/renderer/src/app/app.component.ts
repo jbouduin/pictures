ddf1ef24d984b88f209a0bc347fea67fb5902941
Johan Bouduin <johan@der-e-coach.de>
2020-04-24 22:36:15 +0200

    Initial commit

---
+++ src/renderer/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IpcService } from './ipc.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-electron-boilerplate';
+
+  constructor(private ipcService: IpcService) {
+  }
+
+  clickDevTools() {
+    console.log('test');
+    this.ipcService.openDevTools();
+  }
+}
---
+++ src/renderer/src/app/components/component2/component2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public click(): void {
+    // window.open('https://github.com', '_blank', 'nodeIntegration=no')
+    window.open('https://github.com');
+  }
+}
